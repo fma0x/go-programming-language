a4cf5e9acc2003f8d6791e40f8b9b9226ca1a4e3
fma0x <fma0x@proton.me>
2023-12-27 12:17:27 -0300

    primeira versão

---
+++ ch3/basename/basename.go
@@ -0,0 +1,21 @@
+package basename
+
+func basename(s string) string {
+	// descarta a última '/' e tudo oque estiver antes
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+
+	// preserva tudo o que estiver antes do último '.'
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+
+	return s
+}
